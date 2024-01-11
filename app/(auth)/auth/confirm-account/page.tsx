6b836c6bf87d622c582c8d3c236a4eca7bb69357
igmtink <igmtse@gmail.com>
2024-01-11 12:20:48 +0800

    added confirmation account using server component

--- app/(auth)/auth/confirm-account/page.tsx
+++ app/(auth)/auth/confirm-account/page.tsx
@@ -1,5 +1,40 @@
-import { ConfirmAccountForm } from '@/app/(auth)/auth/_components/confirm-account-form'
+// import { ClientConfirmAccountForm } from '@/app/(auth)/auth/_components/client-confirm-account-form'
 
-export default function ConfirmAccount() {
-  return <ConfirmAccountForm />
+// export default function ConfirmAccount() {
+//   return <ClientConfirmAccountForm />
+// }
+
+import Link from 'next/link'
+
+import { BeatLoader } from 'react-spinners'
+
+import { confirmAccount } from '@/actions/confirm-account'
+import { FormError, FormSuccess } from '@/components/auth/form-status'
+import { Card } from '@/components/ui/card'
+
+export default async function ConfirmAccount({
+  searchParams
+}: {
+  searchParams: { token: string }
+}) {
+  const result = await confirmAccount(searchParams.token)
+  return (
+    <Card
+      title='Account Confirmation'
+      description='Confirm your verification'
+      className='max-w-md'
+    >
+      <div className='mb-2 flex w-full items-center justify-center'>
+        {!result.error && !result.success && (
+          <BeatLoader size='6' color='#ffffff' />
+        )}
+        <FormError message={result.error} />
+        <FormSuccess message={result.success} />
+      </div>
+
+      <Link href='/login' className='text-center text-sm'>
+        Back to login
+      </Link>
+    </Card>
+  )
 }
